c22e20375c0e7d6a6ac08add7d247ff8ef754206
Jonathan Linowes <jonathan@parkerhill.com>
2022-05-23 12:26:32 -0400

    get html

--- api/index.ts
+++ api/index.ts
@@ -1,30 +1,67 @@
-import { IncomingMessage, ServerResponse } from 'http';
-import { parseRequest } from './_lib/parser';
-import { getScreenshot } from './_lib/chromium';
-import { getHtml } from './_lib/template';
+import { IncomingMessage, ServerResponse } from "http";
+import { parseRequest } from "./_lib/parser";
+import { getScreenshot } from "./_lib/chromium";
+import { getHtml } from "./_lib/template";
+
+import { parse } from "url";
 
 const isDev = !process.env.AWS_REGION;
-const isHtmlDebug = process.env.OG_HTML_DEBUG === '1';
+const isHtmlDebug = process.env.OG_HTML_DEBUG === "1";
 
-export default async function handler(req: IncomingMessage, res: ServerResponse) {
-    try {
-        const parsedReq = parseRequest(req);
-        const html = getHtml(parsedReq);
-        if (isHtmlDebug) {
-            res.setHeader('Content-Type', 'text/html');
-            res.end(html);
-            return;
-        }
-        const { fileType } = parsedReq;
-        const file = await getScreenshot(html, fileType, isDev);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', `image/${fileType}`);
-        res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
-        res.end(file);
-    } catch (e) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>');
-        console.error(e);
+export default async function handler(
+  req: IncomingMessage,
+  res: ServerResponse
+) {
+  try {
+    if (req.method === "GET") {
+      //   await original_vercel_og_image(req, res);
+    } else if (req.method === "POST") {
+      // for my internal usage, pass raw html
     }
+  } catch (e) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Internal Error</h1><p>Sorry, there was a problem</p>");
+    console.error(e);
+  }
+}
+
+async function original_vercel_og_image(
+  req: IncomingMessage,
+  res: ServerResponse
+) {
+  // original vercel/og-image get
+  const parsedReq = parseRequest(req);
+  const html = getHtml(parsedReq);
+  if (isHtmlDebug) {
+    res.setHeader("Content-Type", "text/html");
+    res.end(html);
+    return;
+  }
+  const { fileType } = parsedReq;
+  const file = await getScreenshot(html, fileType, isDev);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", `image/${fileType}`);
+  res.setHeader(
+    "Cache-Control",
+    `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
+  );
+  res.end(file);
+}
+
+async function html_to_image(req: any, res: ServerResponse) {
+  //   const html = req.body.html;
+  const { pathname, query } = parse(req.url || "/", true);
+  const { html } = query || {};
+  console.log("html", html);
+  const isDev = false;
+  const fileType = "png";
+  const file = await getScreenshot(html as string, fileType, isDev);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", `image/${fileType}`);
+  res.setHeader(
+    "Cache-Control",
+    `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
+  );
+  res.end(file);
 }
